d0837cbbe4f305bdcec80566909d384368a09c7d
adba98 <oscar.adba@gmail.com>
2022-11-12 16:07:47 -0500

    Type interference

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        var myVariable = 10;
+        System.out.println("myVariable = " + myVariable);
+
+        var myVariable2 = "string";
+        System.out.println("myVariable2 = " + myVariable2);
     }
 }
