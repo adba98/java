02abe64f78361db7aa8ca6d78d89c97df9d5cd05
adba98 <oscar.adba@gmail.com>
2022-11-14 16:10:50 -0500

    Arrays

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,42 @@
 import domain.Client;
 import domain.Employed;
+import domain.Person;
 
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Employed employed = new Employed("Oscar", 500);
-        System.out.println(employed.toString());
+        int ages[] = new int[3];
 
-        Client client = new Client("Adrian", 'F', 25, "Cr 88", new Date(), true);
-        System.out.println(client.toString());
+        ages[0] = 10;
+        System.out.println("ages[0] = " + ages[0]);
+
+        // Runtime error
+        // ages[3] = 5;
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println("Ages " + i + ": " + ages[i]);
+        }
+
+        Person people[] = new Person[2];
+        people[0] = new Person("Oscar");
+        people[1] = new Person("Adrian");
+
+        for (int i = 0; i < people.length; i++) {
+            System.out.println("People " + i + ": " + people[i]);
+        }
+
+        String fruits[] = {"Orange", "Banana", "Strawberry"};
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("Fruits " + i + ": " + fruits[i]);
+        }
+
+        // int numbers[][] = new int[3][2];
+        int numbers[][] = {{1, 2, 3}, {1, 2, 3}};
+        for (int row = 0; row < numbers.length; row++) {
+            for (int column = 0; column < numbers[row].length; column++) {
+                System.out.println("Numbers" + row + "-" + column + ":" + numbers[row][column]);
+            }
+        }
     }
 }
